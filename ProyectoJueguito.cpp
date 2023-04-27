92a6272135ed5c31a9eb4bdd409f4c23f7ff6677
TocheMansion123 <72283818+TocheMansion123@users.noreply.github.com>
2023-04-27 15:17:53 -0500

    Initial commit

---
+++ ProyectoJueguito.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include "iostream"
+
+using namespace System;
+using namespace std;
+
+int main()
+{
+    //Variables
+    int dado;
+    //condicional
+    //Loop
+
+    return 0;
+}
