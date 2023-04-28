ef6492f680032ed45257d6ebd7f1490ed9b0a249
TocheMansion123 <72283818+TocheMansion123@users.noreply.github.com>
2023-04-28 09:58:43 -0500

    Funciona mis dados c:
    Se implementó un case para cada dado para que se muestre los dados que serán múltiplos de nuestro puntaje final c:

--- ProyectoJueguito.cpp
+++ ProyectoJueguito.cpp
@@ -4,6 +4,76 @@
 using namespace System;
 using namespace std;
 
+void mostrarDados(int num) {
+    switch (num) {
+    case 1:
+        cout << endl << "    +--------------+  ";
+        cout << endl << "  /               /|";
+        cout << endl << " +---------------+ |";
+        cout << endl << " |               | |";
+        cout << endl << " |               | |";
+        cout << endl << " |       *       | |";
+        cout << endl << " |               | | ";
+        cout << endl << " |               |/";
+        cout << endl << " +---------------+";
+        break;
+    case 2:
+        cout << endl << "    +--------------+  ";
+        cout << endl << "  /               /|";
+        cout << endl << " +---------------+ |";
+        cout << endl << " |  *            | |";
+        cout << endl << " |               | |";
+        cout << endl << " |               | |";
+        cout << endl << " |               | | ";
+        cout << endl << " |            *  |/";
+        cout << endl << " +---------------+";
+        break;
+    case 3:
+        cout << endl << "    +--------------+  ";
+        cout << endl << "  /               /|";
+        cout << endl << " +---------------+ |";
+        cout << endl << " |  *            | |";
+        cout << endl << " |               | |";
+        cout << endl << " |       *       | |";
+        cout << endl << " |               | | ";
+        cout << endl << " |            *  |/";
+        cout << endl << " +---------------+";
+        break;
+    case 4:
+        cout << endl << "    +--------------+  ";
+        cout << endl << "  /               /|";
+        cout << endl << " +---------------+ |";
+        cout << endl << " |  *         *  | |";
+        cout << endl << " |               | |";
+        cout << endl << " |               | |";
+        cout << endl << " |               | | ";
+        cout << endl << " |  *         *  |/";
+        cout << endl << " +---------------+";
+        break;
+    case 5:
+        cout << endl << "    +--------------+  ";
+        cout << endl << "  /               /|";
+        cout << endl << " +---------------+ |";
+        cout << endl << " |  *         *  | |";
+        cout << endl << " |               | |";
+        cout << endl << " |       *       | |";
+        cout << endl << " |               | | ";
+        cout << endl << " |  *         *  |/";
+        cout << endl << " +---------------+";
+        break;
+    case 6:
+        cout << endl << "    +--------------+  ";
+        cout << endl << "  /               /|";
+        cout << endl << " +---------------+ |";
+        cout << endl << " |  *         *  | |";
+        cout << endl << " |               | |";
+        cout << endl << " |  *         *  | |";
+        cout << endl << " |               | | ";
+        cout << endl << " |  *         *  |/";
+        cout << endl << " +---------------+";
+        break;
+    }
+}
 
 int randomice(int dado) {
     dado = rand() % (6) + 1;
@@ -32,7 +102,7 @@ void instrucciones(){
     cout << "¿Listo para comenzar? ¡Que empiece la diversión!" << endl;
 }
 int main()
-{
+{menu();
     srand(time(0));
     //Variables
     int dados = 3;
@@ -40,8 +110,7 @@ int main()
     int i = 0;
     int jugadas = 10;
     int j1, j2, j3, j4, j5, j6, j7, j8, j9, j10;
-    int puntuajefinaljg;
-    int puntuajefinalpc;
+    int puntuajefinaljg, puntuajefinalpc, puntajejg, puntajepc;
     int partida=1;
     
     int opcion;
@@ -51,22 +120,26 @@ int main()
     cout << "2-Instrucciones" << endl;
     cout << "3-Salir" << endl;
     cin >> opcion;
-
     //condicional
 
      
     switch (opcion) 
     {
-        case 1:   while (1) {
-                cout << "Es tu turno de tirar los dados" << endl;
-                dado1 = randomice(dado1);
-                dado2 = randomice(dado2);
-                dado3 = randomice(dado3);
+        case 1:
+            
+            while (dados>0 && jugadas>0) {
+            cout << "Es tu turno de tirar los dados" << endl;
+            dado1 = randomice(dado1);
+            dado2 = randomice(dado2);
+            dado3 = randomice(dado3);
+            puntajejg = dado1 * dado2 * dado3;
+            mostrarDados(dado1);
+            mostrarDados(dado2);
+            mostrarDados(dado3);
+            cout << "Tu puntaje de esta ronda es: " << puntajejg << endl;
 
-                
+            jugadas--;
             }
-
-
 
 
             break;
@@ -82,7 +155,7 @@ int main()
             cout << "Ingrese un numero de las opciones" <<endl;
             break;
     }
-    //
+    
 
     
     return 0;
